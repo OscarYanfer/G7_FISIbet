ec4bdf7704e2c2457189adcde81880efff398056
wilfredo huallpartupa <wilfredo.huallpartupa@panbox.io>
2023-12-02 09:45:29 -0500

    Cambio en el response del login

--- back/fisibet-usuario/src/main/java/com/microservicio/fisibet/aplication/response/AuthResponse.java
+++ back/fisibet-usuario/src/main/java/com/microservicio/fisibet/aplication/response/AuthResponse.java
@@ -1,9 +1,24 @@
 package com.microservicio.fisibet.aplication.response;
 
+import java.time.LocalDateTime;
+
 public class AuthResponse {
+    private Integer id;
     private String username;
-    private String profile;
+    private String email;
+    private String password;
     private Integer conectado;
+    private String profile;
+    private LocalDateTime registeredOn;
+    private LocalDateTime updatedOn;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
@@ -11,6 +26,30 @@ public class AuthResponse {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getConectado() {
+        return conectado;
+    }
+
+    public void setConectado(Integer conectado) {
+        this.conectado = conectado;
     }
 
     public String getProfile() {
@@ -21,11 +60,19 @@ public class AuthResponse {
         this.profile = profile;
     }
 
-    public Integer getConectado() {
-        return conectado;
+    public LocalDateTime getRegisteredOn() {
+        return registeredOn;
     }
 
-    public void setConectado(Integer conectado) {
-        this.conectado = conectado;
+    public void setRegisteredOn(LocalDateTime registeredOn) {
+        this.registeredOn = registeredOn;
+    }
+
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
     }
 }
