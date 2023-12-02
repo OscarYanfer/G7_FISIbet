de88b60448a02f3acf543252f13882d08ff14af3
OscarYanfer <oscar.yanfer@unmsm.edu.pe>
2023-12-02 15:24:17 +0000

    Se corrige el nombre de la tabla usersession

--- back/fisibet-usuario/src/main/java/com/microservicio/fisibet/infraestructure/model/SessionUserModel.java
+++ back/fisibet-usuario/src/main/java/com/microservicio/fisibet/infraestructure/model/SessionUserModel.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "sessionUser")
+@Table(name = "sessionuser")
 public class SessionUserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
